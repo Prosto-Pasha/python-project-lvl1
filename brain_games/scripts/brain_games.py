3c1c3783f24936c897acbdd5b5fcad42f4f16205
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-18 22:40:19 +0300

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
-
 #!/usr/bin/env python3
+
+from brain_games.cli import welcome_user
+
 
 def print_hello():
     print('Welcome to Brain Games!')
+    welcome_user()
 
 def main():
     print_hello()
