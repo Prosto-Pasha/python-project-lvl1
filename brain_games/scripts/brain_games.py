8b06b58ca491a4353ecfabd0ceda0da572b687b6
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-19 23:51:42 +0300

    update brain_games.py for linter

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,15 +1,20 @@
-#!/usr/bin/env python3
+"""Основной модуль проекта."""
+
+# !/usr/bin/env python3
 
 from brain_games.cli import welcome_user
 
 
 def print_hello():
+    """Выводим приветствие."""
     print('Welcome to Brain Games!')
     welcome_user()
 
+
 def main():
+    """Основная фанкция проекта."""
     print_hello()
+
 
 if __name__ == '__main__':
     main()
-
