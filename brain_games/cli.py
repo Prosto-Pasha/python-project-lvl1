c86972a0bb45cc68d13e67d097acbd9f7ab2cfd9
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-19 23:51:04 +0300

    update cli.py for linter

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,9 @@
+"""Знакомимся с игроком и приветствуем его."""
+
 import prompt
 
 
 def welcome_user():
+    """Знакомимся с игроком и приветствуем его."""
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
+    print('Hello, {0}!'.format(name))
