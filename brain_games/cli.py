cefa5a7dadd5e4d12e47c2d8e4c6ac4f953882f0
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-20 23:28:53 +0300

    update cli.py add return

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,6 +4,12 @@ import prompt
 
 
 def welcome_user():
-    """Знакомимся с игроком и приветствуем его."""
+    """
+    Знакомимся с игроком и приветствуем его.
+
+    Returns:
+            string : player name
+    """
     name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(name))
+    return name
