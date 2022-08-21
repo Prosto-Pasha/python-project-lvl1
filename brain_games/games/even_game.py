ad583bcd32237d1009fb08bd21ff5dbebbc19cae
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-21 23:12:47 +0300

    add brain_games/games/even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -14,7 +14,8 @@ def start_game(user_name):
     """
     number_of_correct_answers = 0  # Счётчик правильных ответов
     number_of_rounds = 3  # Число раундов игры, необходимое для победы
-    while number_of_correct_answers < number_of_rounds:  # Если правильных ответов меньше трёх, то продолжаем игру
+    game_is_over = False
+    while number_of_correct_answers < number_of_rounds or game_is_over:  # Если правильных ответов меньше трёх, то продолжаем игру
         number = randint(0, 1000)  # Случайное целое число от 0 до 1000
         common_proc.ask_question(number)  # Задаём вопрос пользователю
         user_answer = common_proc.find_answer('string')  # Узнаём ответ пользователя
@@ -25,6 +26,6 @@ def start_game(user_name):
         else:
             common_proc.wrong_answer_report(user_answer, correct_answer)  # Сообщим о неправильном ответе
             print("Let's try again, {0}!".format(user_name))
-            break  # Завершаем игру при неправильном ответе
+            game_is_over = True  # Завершаем игру при неправильном ответе
     if number_of_correct_answers == 3:  # Поздравим пользователя при трёх правильных ответах
         common_proc.greet_user(user_name)
