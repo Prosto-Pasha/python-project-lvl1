74179306535e8d793eef9540a948623ef7297fac
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-21 19:29:54 +0300

    add brain_games/games/

---
+++ brain_games/games/calc_game.py
@@ -0,0 +1,82 @@
+"""Логика игры 'Калькулятор'."""
+
+from random import choice, randint
+
+from brain_games import common_proc
+
+
+def get_numbers(current_oper):
+    """
+    Получить параметры для текущей арифметической опреации.
+
+    Parameters:
+        current_oper : строка, текущая арифметическая операция.
+
+    Returns:
+        tuple : кортеж с двумя параметрами и правильным ответом для текущей операции
+
+    Для операции сложения '+' выбираются два случайных числа от 0 до 100.
+
+    Для операции вычитания '-' выбираются два случайных числа от 0 до 100.
+    Первое число должно быть больше второго.
+
+    Для операции умножения '*' выбираются два случайных числа.
+    Первое число от 0 до 100. Второе число от 0 до 10.
+
+    Для операции деления '/' выбираются два случайных числа.
+    Первое число от 0 до 50. Второе число от 0 до 10.
+    Делимое определяем умножением первого и второго чисел.
+    Второе число будет делителем.
+    Возвращаем делимое и второе число.
+    """
+    max_number = 100  # Максимальное значение числа
+    max_number2 = 10  # Максимальное значение числа для умножения и деления
+    max_number3 = 50  # Максимальное значение числа для деления
+    param1 = randint(0, max_number)  # Первый элемент для результата функции (от 0 до 100)
+    param2 = randint(0, max_number)  # Второй элемент для результата функции (от 0 до 100)
+    param3 = randint(0, max_number2)  # Элемент для функций умножения и деления (от 0 до 10)
+    correct_answer = 0  # Правильный результат функции
+    if current_oper == '+':
+        correct_answer = param1 + param2
+    elif current_oper == '-':
+        if param1 < param2:
+            param1, param2 = param2, param1
+        correct_answer = param1 - param2
+    elif current_oper == '*':
+        param2 = param3
+        correct_answer = param1 * param2
+    elif current_oper == '/':
+        expr_result = randint(0, max_number3)  # Число от 0 до 50
+        param2 = param3
+        param1 = expr_result * param2
+        correct_answer = param1 / param2
+    return (param1, param2, correct_answer)
+
+
+def start_game(user_name):
+    """
+    Игра 'Калькулятор'.
+
+    Parameters:
+        user_name : строка, имя игрока.
+    """
+    number_of_correct_answers = 0  # Счётчик правильных ответов
+    number_of_rounds = 3  # Число раундов игры, необходимое для победы
+    opers = ('+', '-', '*', '/')  # Арифмитические операции игры
+    while number_of_correct_answers < number_of_rounds:  # Если правильных ответов меньше трёх, то продолжаем игру
+        current_oper = choice(opers)  # Текущая арифметическая операция
+        first_number, second_number, correct_answer = get_numbers(current_oper)  # Получим значения для текущей операции
+        first_number_str = str(first_number)
+        second_number_str = str(second_number)
+        question_text = '{0} {1} {2}'.format(first_number_str, current_oper, second_number_str)
+        common_proc.ask_question(question_text)  # Задаём вопрос пользователю
+        user_answer = common_proc.find_answer('integer')  # Узнаём ответ пользователя
+        if common_proc.compare_answers(user_answer, correct_answer):  # Сравниваем ответ игрока и правильный ответ
+            print('Correct!')
+            number_of_correct_answers += 1  # Увеличиваем счётчик правильных ответов
+        else:
+            common_proc.wrong_answer_report(user_answer, correct_answer)  # Сообщим о неправильном ответе
+            print("Let's try again, {0}!".format(user_name))
+            break  # Завершаем игру при неправильном ответе
+    if number_of_correct_answers == 3:  # Поздравим пользователя при трёх правильных ответах
+        common_proc.greet_user(user_name)
---
+++ brain_games/games/even_game.py
@@ -0,0 +1,30 @@
+"""Логика игры 'Проверка на чётность'."""
+
+from random import randint
+
+from brain_games import common_proc
+
+
+def start_game(user_name):
+    """
+    Игра 'Проверка на чётность'.
+
+    Parameters:
+        user_name : строка, имя игрока.
+    """
+    number_of_correct_answers = 0  # Счётчик правильных ответов
+    number_of_rounds = 3  # Число раундов игры, необходимое для победы
+    while number_of_correct_answers < number_of_rounds:  # Если правильных ответов меньше трёх, то продолжаем игру
+        number = randint(0, 1000)  # Случайное целое число от 0 до 1000
+        common_proc.ask_question(number)  # Задаём вопрос пользователю
+        user_answer = common_proc.find_answer('string')  # Узнаём ответ пользователя
+        correct_answer = 'yes' if number % 2 == 0 else 'no'  # Вычисляем правильный ответ
+        if common_proc.compare_answers(user_answer, correct_answer):  # Сравниваем ответ игрока и правильный ответ
+            print('Correct!')
+            number_of_correct_answers += 1  # Увеличиваем счётчик правильных ответов
+        else:
+            common_proc.wrong_answer_report(user_answer, correct_answer)  # Сообщим о неправильном ответе
+            print("Let's try again, {0}!".format(user_name))
+            break  # Завершаем игру при неправильном ответе
+    if number_of_correct_answers == 3:  # Поздравим пользователя при трёх правильных ответах
+        common_proc.greet_user(user_name)
