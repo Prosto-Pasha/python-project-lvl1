94f65d8c0a3a52cafa9823f2534386431e91142e
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-22 21:04:42 +0300

    refactoring brain_games/games/even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -14,18 +14,11 @@ def start_game(user_name):
     """
     number_of_correct_answers = 0  # Счётчик правильных ответов
     number_of_rounds = 3  # Число раундов игры, необходимое для победы
-    game_is_over = False
-    while number_of_correct_answers < number_of_rounds or game_is_over:  # Если правильных ответов меньше трёх, то продолжаем игру
+    while number_of_correct_answers < number_of_rounds:  # Если правильных ответов меньше трёх, то продолжаем игру
         number = randint(0, 1000)  # Случайное целое число от 0 до 1000
         common_proc.ask_question(number)  # Задаём вопрос пользователю
         user_answer = common_proc.find_answer('string')  # Узнаём ответ пользователя
         correct_answer = 'yes' if number % 2 == 0 else 'no'  # Вычисляем правильный ответ
-        if common_proc.compare_answers(user_answer, correct_answer):  # Сравниваем ответ игрока и правильный ответ
-            print('Correct!')
-            number_of_correct_answers += 1  # Увеличиваем счётчик правильных ответов
-        else:
-            common_proc.wrong_answer_report(user_answer, correct_answer)  # Сообщим о неправильном ответе
-            print("Let's try again, {0}!".format(user_name))
-            game_is_over = True  # Завершаем игру при неправильном ответе
+        number_of_correct_answers += common_proc.check_answer(user_answer, correct_answer, user_name)
     if number_of_correct_answers == 3:  # Поздравим пользователя при трёх правильных ответах
         common_proc.greet_user(user_name)
