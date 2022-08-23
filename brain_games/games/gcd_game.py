84559677368b22049a40895dc21b08318f5df6a2
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-23 22:59:43 +0300

    refactoring brain_games/games/gcd_game.py

--- brain_games/games/gcd_game.py
+++ brain_games/games/gcd_game.py
@@ -7,7 +7,9 @@ from brain_games import common_proc
 
 def euqlid_gcd(number1, number2):
     """
-    Определяем наибольший общий делитель чисел number1 и number2 по методу Евклида.
+    Определяем наибольший общий делитель чисел number1 и number2.
+
+    Методом Евклида.
 
     Parameters:
         number1 : число, первый параметр.
@@ -33,14 +35,22 @@ def start_game(user_name):
     """
     number_of_correct_answers = 0  # Счётчик правильных ответов
     number_of_rounds = 3  # Число раундов игры, необходимое для победы
-    while number_of_correct_answers < number_of_rounds:  # Если правильных ответов меньше трёх, то продолжаем игру
+    # Если правильных ответов меньше трёх, то продолжаем игру
+    while number_of_correct_answers < number_of_rounds:
         max_number = 150  # Максимальное число
         number1 = randint(0, max_number)  # Случайное целое число от 1 до 150
         number2 = randint(0, max_number)  # Случайное целое число от 1 до 150
         question_text = '{0} {1}'.format(number1, number2)
         common_proc.ask_question(question_text)  # Задаём вопрос пользователю
-        user_answer = common_proc.find_answer('integer')  # Узнаём ответ пользователя
-        correct_answer = euqlid_gcd(number1, number2)  # Вычисляем правильный ответ
-        number_of_correct_answers += common_proc.check_answer(user_answer, correct_answer, user_name)
-    if number_of_correct_answers == 3:  # Поздравим пользователя при трёх правильных ответах
+        # Узнаём ответ пользователя
+        user_answer = common_proc.find_answer('integer')
+        # Вычисляем правильный ответ
+        correct_answer = euqlid_gcd(number1, number2)
+        number_of_correct_answers += common_proc.check_answer(
+            user_answer,
+            correct_answer,
+            user_name,
+        )
+    # Поздравим пользователя при трёх правильных ответах
+    if number_of_correct_answers == 3:
         common_proc.greet_user(user_name)
