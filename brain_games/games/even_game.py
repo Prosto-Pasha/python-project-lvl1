2a6eeb6fd8d924abf76b5357952883ff99f86291
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-23 22:59:28 +0300

    refactoring brain_games/games/even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -14,11 +14,19 @@ def start_game(user_name):
     """
     number_of_correct_answers = 0  # Счётчик правильных ответов
     number_of_rounds = 3  # Число раундов игры, необходимое для победы
-    while number_of_correct_answers < number_of_rounds:  # Если правильных ответов меньше трёх, то продолжаем игру
+    # Если правильных ответов меньше трёх, то продолжаем игру
+    while number_of_correct_answers < number_of_rounds:
         number = randint(0, 1000)  # Случайное целое число от 0 до 1000
         common_proc.ask_question(number)  # Задаём вопрос пользователю
-        user_answer = common_proc.find_answer('string')  # Узнаём ответ пользователя
-        correct_answer = 'yes' if number % 2 == 0 else 'no'  # Вычисляем правильный ответ
-        number_of_correct_answers += common_proc.check_answer(user_answer, correct_answer, user_name)
-    if number_of_correct_answers == 3:  # Поздравим пользователя при трёх правильных ответах
+        # Узнаём ответ пользователя
+        user_answer = common_proc.find_answer('string')
+        # Вычисляем правильный ответ
+        correct_answer = 'yes' if number % 2 == 0 else 'no'
+        number_of_correct_answers += common_proc.check_answer(
+            user_answer,
+            correct_answer,
+            user_name,
+        )
+    # Поздравим пользователя при трёх правильных ответах
+    if number_of_correct_answers == 3:
         common_proc.greet_user(user_name)
