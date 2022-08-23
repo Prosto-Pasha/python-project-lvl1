ecb323b9a2e4919c1394ebfc861f4c621f587d2e
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-23 23:00:00 +0300

    refactoring brain_games/games/prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -30,7 +30,8 @@ def get_round():
     Получить число и правильный ответ для очередного раунда.
 
     Returns:
-        tuple : кортеж с двумя элементами. Число - вопрос и строка - правильный ответ ('yes' или 'no').
+        tuple : кортеж с двумя элементами.
+        Число - вопрос и строка - правильный ответ ('yes' или 'no').
     """
     max_number = 1000  # Максимальное число для вопроса
     question_number = randint(1, max_number)  # Число - вопрос от 1 до 1000
@@ -47,10 +48,17 @@ def start_game(user_name):
     """
     number_of_correct_answers = 0  # Счётчик правильных ответов
     number_of_rounds = 3  # Число раундов игры, необходимое для победы
-    while number_of_correct_answers < number_of_rounds:  # Если правильных ответов меньше трёх, то продолжаем игру
+    # Если правильных ответов меньше трёх, то продолжаем игру
+    while number_of_correct_answers < number_of_rounds:
         question_text, correct_answer = get_round()
         common_proc.ask_question(question_text)  # Задаём вопрос пользователю
-        user_answer = common_proc.find_answer('string')  # Узнаём ответ пользователя
-        number_of_correct_answers += common_proc.check_answer(user_answer, correct_answer, user_name)
-    if number_of_correct_answers == 3:  # Поздравим пользователя при трёх правильных ответах
+        # Узнаём ответ пользователя
+        user_answer = common_proc.find_answer('string')
+        number_of_correct_answers += common_proc.check_answer(
+            user_answer,
+            correct_answer,
+            user_name,
+        )
+    # Поздравим пользователя при трёх правильных ответах
+    if number_of_correct_answers == 3:
         common_proc.greet_user(user_name)
