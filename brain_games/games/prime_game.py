49fa151c8c4fa991f941cf380a670c067c6b870c
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-25 22:34:00 +0300

    refactoring brain_games/games/prime_game.py

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -16,8 +16,10 @@ def get_question_answer():
     Returns:
         tuple : кортеж с текстом вопроса и правильным ответом
     """
-    question_text, correct_answer = get_round()
-    return (question_text, correct_answer)
+    max_number = 1000  # Максимальное число для вопроса
+    question_number = randint(1, max_number)  # Число - вопрос от 1 до 1000
+    correct_answer = is_simple(question_number)
+    return (question_number, correct_answer)
 
 
 def is_simple(number):
@@ -37,17 +39,3 @@ def is_simple(number):
             return 'no'
         index += 1
     return 'yes'
-
-
-def get_round():
-    """
-    Получить число и правильный ответ для очередного раунда.
-
-    Returns:
-        tuple : кортеж с двумя элементами.
-        Число - вопрос и строка - правильный ответ ('yes' или 'no').
-    """
-    max_number = 1000  # Максимальное число для вопроса
-    question_number = randint(1, max_number)  # Число - вопрос от 1 до 1000
-    correct_answer = is_simple(question_number)
-    return (question_number, correct_answer)
