3c1f6b5bd4f7385ae3f6e3a3d02a2d5ffc35751a
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-25 22:22:53 +0300

    refactoring brain_games/scripts/brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,14 +3,12 @@
 # !/usr/bin/env python3
 
 from brain_games import common_proc
-from brain_games.games import prime_game
+from brain_games.games import prime_game as game
 
 
 def main():
-    """Основная функция игры 'Простое ли число?'."""
-    game_rules = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-    user_name = common_proc.welcome_user(game_rules)
-    prime_game.start_game(user_name)
+    """Запуск игры 'Простое ли число?'."""
+    common_proc.start_game(game)
 
 
 if __name__ == '__main__':
