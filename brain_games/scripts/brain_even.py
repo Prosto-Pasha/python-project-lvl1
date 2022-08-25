93b0cde40c7e2ba180b50faab7d3c57a488b68ca
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-25 22:22:16 +0300

    refactoring brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,14 +3,12 @@
 # !/usr/bin/env python3
 
 from brain_games import common_proc
-from brain_games.games import even_game
+from brain_games.games import even_game as game
 
 
 def main():
-    """Основная функция игры 'Проверка на чётность'."""
-    game_rules = 'Answer "yes" if the number is even, otherwise answer "no".'
-    user_name = common_proc.welcome_user(game_rules)
-    even_game.start_game(user_name)
+    """Запуск игры 'Проверка на чётность'."""
+    common_proc.start_game(game)
 
 
 if __name__ == '__main__':
