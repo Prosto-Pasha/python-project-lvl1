172d5cc95eec8a21514f099e5357bd5c410554bf
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-25 22:22:35 +0300

    refactoring brain_games/scripts/brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -3,14 +3,12 @@
 # !/usr/bin/env python3
 
 from brain_games import common_proc
-from brain_games.games import gcd_game
+from brain_games.games import gcd_game as game
 
 
 def main():
-    """Основная функция игры 'Калькулятор'."""
-    game_rules = 'Find the greatest common divisor of given numbers.'
-    user_name = common_proc.welcome_user(game_rules)
-    gcd_game.start_game(user_name)
+    """Запуск игры 'Наибольший общий делитель'."""
+    common_proc.start_game(game)
 
 
 if __name__ == '__main__':
