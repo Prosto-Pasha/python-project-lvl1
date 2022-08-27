fa0dde9bcf98ba6197b29fffd8fa07f27cf49353
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-27 12:16:54 +0300

    refactoring brain_games/games/prjgression_game.py

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -4,8 +4,14 @@ from random import randint
 
 # Правила игры
 RULES = 'What number is missing in the progression?'
-# Тип ответа (строка или число)
-ANSWER_TYPE = 'integer'
+MIN_PROGRESSION_NUMBER = 5  # Минимальная длина прогрессии
+MAX_PROGRESSION_NUMBER = 10  # Максимальная длина прогрессии
+# Минимальное значение числа в прогрессии,
+# минимальный шаг прогрессии,
+# минимальное значение для поиска индекса правильного ответа
+MIN_NUMBER = 1
+MAX_NUMBER = 20  # Максимальное значение первого числа в прогрессии
+MAX_STEP = 10  # Максимальный шаг прогрессии
 
 
 def get_question_answer():
@@ -16,20 +22,24 @@ def get_question_answer():
         tuple : кортеж с двумя элементами.
             Строка с вопросом и число - правильный ответ.
     """
-    length_of_progression = randint(5, 10)  # Длина прогрессии от 5 до 10 чисел
-    # Максимальное значение первого числа в прогрессиии
-    max_first_number = 20
-    max_step = 10  # Максимальный шаг прогрессии
-    # Первое число в прогрессии от 1 до 20
-    first_number = randint(1, max_first_number)
-    step = randint(1, max_step)  # Шаг прогрессии от 1 до 10
+    # Длина прогрессии
+    length_of_progression = randint(
+        MIN_PROGRESSION_NUMBER,
+        MAX_PROGRESSION_NUMBER,
+    )
+    # Первое число в прогрессии
+    first_number = randint(MIN_NUMBER, MAX_NUMBER)
+    # Шаг прогрессии
+    step = randint(MIN_NUMBER, MAX_STEP)
     # Последнее число в прогрессии
     last_number = first_number + length_of_progression * step
     # Индекс загадываемого числа
-    lucky_index = randint(1, length_of_progression) - 1
+    lucky_index = randint(MIN_NUMBER, length_of_progression) - 1
     progression_list = list(range(first_number, last_number, step))
-    correct_answer = progression_list[lucky_index]  # Правильный ответ
-    progression_list[lucky_index] = '..'  # Скрываем правильный ответ
+    # Правильный ответ
+    correct_answer = progression_list[lucky_index]
+    # Скрываем правильный ответ
+    progression_list[lucky_index] = '..'
     # Преобразуем список в строку
     question_text = ' '.join(map(str, progression_list))
-    return (question_text, correct_answer)
+    return question_text, str(correct_answer)
