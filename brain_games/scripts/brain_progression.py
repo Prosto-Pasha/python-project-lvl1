95c6cd037a64ed84989985b3219c88d54048842d
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-29 22:49:59 +0300

    refactoring brain_games/games/brain_progression.py

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -2,13 +2,13 @@
 
 # !/usr/bin/env python3
 
-from brain_games import common_proc
+from brain_games import engine
 from brain_games.games import progression_game as game
 
 
 def main():
     """Запуск игры 'Арифметическая прогрессия'."""
-    common_proc.start_game(game)
+    engine.run(game)
 
 
 if __name__ == '__main__':
