736ebdc074ddf1de44d0269011f9f8d5d9f705af
Pavel Belyavskiy <prostopasha@gmail.com>
2022-08-30 20:50:11 +0300

    refactoring brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -18,9 +18,8 @@ def run(game):
     player_name = prompt.string('May I have your name? ')
     print('Hello, {0}!'.format(player_name))
     print(game.RULES)
-    number_of_correct_answers = 0
     # Если правильных ответов меньше необходимого, то продолжаем игру
-    while number_of_correct_answers < NUMBER_OF_ROUNDS:
+    for _ in range(NUMBER_OF_ROUNDS):
         # Получим текст вопроса и правильный ответ
         question_text, correct_answer = game.get_question_answer()
         print('Question: {0}'.format(question_text))
@@ -28,12 +27,10 @@ def run(game):
         if player_answer == correct_answer:
             # Правильный ответ
             print('Correct!')
-            number_of_correct_answers += CORRECT_ANSWER_WEIGHT
         else:
             # Неправильный ответ
             wrong_answer = "'{0}' is wrong answer ;(. Correct answer was '{1}'"
             print(wrong_answer.format(player_answer, correct_answer))
             print("Let's try again, {0}!".format(player_name))
-            number_of_correct_answers += WRONG_ANSWER_WEIGHT
-    if number_of_correct_answers == NUMBER_OF_ROUNDS:
-        print('Congratulations, {0}!'.format(player_name))
+            return
+    print('Congratulations, {0}!'.format(player_name))
